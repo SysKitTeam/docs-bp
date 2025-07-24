0cc2b8b9c2edcf3b2f38dffa095c3846b9da151f
imesaric <igor.mesaric.w@outlook.com>
2025-07-24 12:56:10 +0200

    favicon and title update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
+  title: 'SharePoint Best Practices',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 

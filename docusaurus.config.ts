874346d12b8e9aa74d7c76320e233aeb56bc8421
imesaric <igor.mesaric.w@outlook.com>
2025-07-24 16:47:13 +0200

    announcement test

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -58,6 +58,20 @@ const config: Config = {
   ],
 
   themeConfig: {
+    // Comment if not needed
+    announcementBar: {
+      id: 'power_platform_promotion',
+      content:
+        'Get full Power Platform visibility with Syskit Point <a target="_blank" rel="noopener noreferrer" href="https://www.syskit.com/use-cases/power-platform-visibility-and-security/">Learn how</a>',
+      backgroundColor: '#5700af',
+      textColor: '#ffffff',
+      isCloseable: true,
+    },
+    colorMode: {
+      defaultMode: 'light',
+      disableSwitch: false,
+      respectPrefersColorScheme: true, // Respect user's color scheme preference
+    },
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {

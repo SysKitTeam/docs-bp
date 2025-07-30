7d0771eb7fddbc5a0ae134ee1c15fb27faf02620
imesaric <igor.mesaric.w@outlook.com>
2025-07-30 10:53:42 +0200

    added code block languages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -149,6 +149,7 @@ const config: Config = {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
+      additionalLanguages: ["bash", "powershell", "json", "typescript"],
     },
   } satisfies Preset.ThemeConfig,
 };
